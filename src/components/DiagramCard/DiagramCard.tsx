aa8116ba9c02e26ace737bc3f888482ed21a5ced
Nishan Bajracharya <nisbaj11@gmail.com>
2024-04-06 21:25:51 +0545

    DCNTH Update diagram card

--- src/components/DiagramCard/DiagramCard.tsx
+++ src/components/DiagramCard/DiagramCard.tsx
@@ -16,10 +16,18 @@ export const DiagramCard = () => {
     // Make a GET request to the JSON Placeholder API
     axios.get(url)
       .then((response) => {
-        response.data = data;
+        // response.data = data;
+        console.log(response.data.data);
                 
         // Handle the response data (e.g., update state)
-        setCards(response.data);
+        const output = [];
+
+        Object.keys(response.data.data).forEach((key) => {
+          response.data.data[key].id = key;
+          output.push(response.data.data[key]);
+        });
+
+        setCards(output);
         setLoading(false);
       })
       .catch((error) => {
@@ -40,32 +48,33 @@ export const DiagramCard = () => {
       {isLoading ? (
         <Loader></Loader>
       ) : (
-        <div>
+        <div className="flex flex-row flex-wrap">
           {cards.map((card: any) => (
             <Link href={{ pathname: "/diagram", query: {data: JSON.stringify(card)} }} key={card.id}>
-              <div className="mt-5" onClick={() => goToEdit(card)}>
-                <div className="flex flex-col justify-between border-slate-950 p-3 h-64 max-w-80">
+              <div className="m-5 min-w-[300px]" onClick={() => goToEdit(card)}>
+                <div className="border-1 rounded border-slate-950 flex flex-col justify-between border-slate-950 p-3 h-64 max-w-80">
                   <div>
-                    <div className="flex flex-row justify-between items-center">
-                      <span className="font-bold">Flow Continue</span>
-                      <HiDotsHorizontal
+                    <div className="">
+                      <p className="font-bold">{card.name}</p>
+                      <p>{card.id}</p>
+                      {/* <HiDotsHorizontal
                         size={24}
                         style={{
                           color: "#000000",
                         }}
-                      />
+                      /> */}
                     </div>
                     <div className="mt-2 flex flex-col justify-start">
                       <span className="flex-wrap text-sm">
-                        asdasd123123asdasd987
+                        
                       </span>
                       <span className="flex-wrap text-sm">
-                        Last Updated On : 07/04/2024, 12:36:33
+                        
                       </span>
                     </div>
                   </div>
 
-                  <div>asdasdasd</div>
+                  <div></div>
                 </div>
               </div>
             </Link>
